7b6200151fea8e0193ee00e7649aa367722ba119
Hieu Vu <hieuvm.0101@gmail.com>
2025-09-12 13:52:45 +0700

    sua khuyen mai

--- admin/khuyenmai/list.php
+++ admin/khuyenmai/list.php
@@ -14,14 +14,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($type == 'PERCENT' && $value > 100) {
             $message = ['type' => 'warning', 'text' => 'Giảm giá % không được lớn hơn 100'];
         } elseif (!empty($code) && !empty($value)) {
-            $sql = "INSERT INTO MaGiamGia (MaCode, GiaTri, Kieu, HanSuDung, SoLanSuDung) 
+            $sql = "INSERT INTO MaGiamGia (MaGiamGia, GiaTri, Kieu, HanSuDung, SoLanSuDung) 
                     VALUES ('$code', '$value', '$type', '$expire', '$limit')";
             if (Database::NonQuery($sql)) {
                 $message = ['type' => 'success', 'text' => 'Thêm thành công'];
             }
         } else {
             $message = ['type' => 'warning'];
-            if (empty($code)) $message['text'][] = 'Mã code không được trống';
+            if (empty($code)) $message['text'][] = 'Mã giảm giá không được trống';
             if (empty($value)) $message['text'][] = 'Giá trị không được trống';
         }
     }
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         } elseif (!empty($value)) {
             $sql = "UPDATE MaGiamGia 
                     SET GiaTri = '$value', Kieu = '$type', HanSuDung = '$expire', SoLanSuDung = '$limit'
-                    WHERE MaCode = '$id'";
+                    WHERE MaGiamGia = '$id'";
             if (Database::NonQuery($sql)) {
                 $message = ['type' => 'success', 'text' => 'Cập nhật thành công'];
             }
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 // Xoá khuyến mãi
 if (isset($_GET['del-id'])) {
     $id = $_GET['del-id'];
-    $sql = "DELETE FROM MaGiamGia WHERE MaCode = '$id'";
+    $sql = "DELETE FROM MaGiamGia WHERE MaGiamGia = '$id'";
     if (Database::NonQuery($sql)) {
         $message = ['type' => 'success', 'text' => 'Xoá thành công'];
     }
@@ -93,7 +93,7 @@ if (isset($_GET['del-id'])) {
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                     </div>
                     <div class="modal-body">
-                        <div class="form-group"><label>Mã code</label><input type="text" name="code" class="form-control"></div>
+                        <div class="form-group"><label>Mã giảm giá</label><input type="text" name="code" class="form-control"></div>
                         <div class="form-group"><label>Giá trị</label><input type="number" name="value" class="form-control"></div>
                         <div class="form-group">
                             <label>Kiểu</label>
@@ -118,7 +118,7 @@ if (isset($_GET['del-id'])) {
         $id = $_GET['edit-id'] ?? '';
         $km = [];
         if ($id != '') {
-            $sql = "SELECT * FROM MaGiamGia WHERE MaCode = '$id'";
+            $sql = "SELECT * FROM MaGiamGia WHERE MaGiamGia = '$id'";
             $km = Database::GetData($sql, ['row'=>0]);
         }
         ?>
@@ -130,7 +130,7 @@ if (isset($_GET['del-id'])) {
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                     </div>
                     <div class="modal-body">
-                        <div class="form-group"><label>Mã code</label><input type="text" value="<?=$km['MaCode']?>" class="form-control" disabled></div>
+                        <div class="form-group"><label>Mã giảm giá</label><input type="text" value="<?=$km['MaGiamGia']?>" class="form-control" disabled></div>
                         <div class="form-group"><label>Giá trị</label><input type="number" name="value" value="<?=$km['GiaTri']?>" class="form-control"></div>
                         <div class="form-group">
                             <label>Kiểu</label>
@@ -155,7 +155,7 @@ if (isset($_GET['del-id'])) {
                 <button type="button" class="btn btn-success mx-2" data-toggle="modal" data-target="#modal-add"><i class="fas fa-plus"></i></button>
                 <form method="GET">
                     <div class="input-group">
-                        <input type="text" name="keyword" placeholder="Mã code" class="form-control">
+                        <input type="text" name="keyword" placeholder="Mã giảm giá" class="form-control">
                         <div class="input-group-append">
                             <button class="btn btn-outline-info"><i class="fas fa-search"></i></button>
                         </div>
@@ -169,7 +169,7 @@ if (isset($_GET['del-id'])) {
                         <table class="table table-hover table-bordered">
                             <thead class="table-warning">
                                 <tr>
-                                    <th>Mã Code</th>
+                                    <th>Mã giảm giá</th>
                                     <th>Giá trị</th>
                                     <th>Kiểu</th>
                                     <th>Hạn sử dụng</th>
@@ -183,23 +183,23 @@ if (isset($_GET['del-id'])) {
                                 $pager = (new Pagination())->get('MaGiamGia', $page, ROW_OF_PAGE);
 
                                 $keyword = $_GET['keyword'] ?? '';
-                                $filter = $keyword ? "WHERE MaCode LIKE '%$keyword%'" : '';
+                                $filter = $keyword ? "WHERE MaGiamGia LIKE '%$keyword%'" : '';
 
-                                $sql = "SELECT * FROM MaGiamGia $filter ORDER BY MaCode DESC LIMIT ".$pager['StartIndex'].', '.ROW_OF_PAGE;
+                                $sql = "SELECT * FROM MaGiamGia $filter ORDER BY MaGiamGia DESC LIMIT ".$pager['StartIndex'].', '.ROW_OF_PAGE;
                                 $data = Database::GetData($sql);
 
                                 if ($data) {
                                     foreach ($data as $row) {
                                         echo '
                                         <tr>
-                                            <td>'.$row['MaCode'].'</td>
+                                            <td>'.$row['MaGiamGia'].'</td>
                                             <td>'.($row['Kieu']=='PERCENT' ? $row['GiaTri'].'%' : number_format($row['GiaTri']).' VND').'</td>
                                             <td>'.($row['Kieu']=='PERCENT' ? '%' : 'VND').'</td>
                                             <td>'.$row['HanSuDung'].'</td>
                                             <td>'.$row['SoLanSuDung'].'</td>
                                             <td>
-                                                <a href="?edit-id='.$row['MaCode'].'" class="btn btn-warning"><i class="fas fa-marker"></i></a>
-                                                <a onclick="removeRow(\''.$row['MaCode'].'\')" class="btn btn-danger"><i class="fas fa-trash-alt"></i></a>
+                                                <a href="?edit-id='.$row['MaGiamGia'].'" class="btn btn-warning"><i class="fas fa-marker"></i></a>
+                                                <a onclick="removeRow(\''.$row['MaGiamGia'].'\')" class="btn btn-danger"><i class="fas fa-trash-alt"></i></a>
                                             </td>
                                         </tr>';
                                     }
